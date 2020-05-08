f97a599d502ca1ccb9d700f4b7354bf26c67be56
jan.werk@gmail.com <jos@glu.nl>
2020-05-08 16:24:31 +0200

    fix in index.js

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-const app = require('app')();
+const app = require('express')();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 const port = 3000;
 
 server.listen(port, () => {
-    console.log(`Server is running on port $(port)`);
+    console.log(`Server is running on port ${port}`);
 });
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirnam + '/public/index.html');
+    res.sendFile(__dirname + '/public/index.html');
 });
 
 io.on('connection', (socket) => {
     console.log('user connected');
-    socket.emit('message', { manny: 'Hey how are you?'});
+    socket.emit('message', { manny: 'hey how are you?'});
     socket.on('another event', (data) => {
         console.log(data);
     })
